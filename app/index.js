0a25251e24037d13dc84bf5f73f9b532c20a3ad8
Panchal <panra16@ca.com>
2018-12-27 12:49:29 +0800

    React skeleton app

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends React.Component {
+  render() {
+    return <div>Hello React Training!</div>;
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
